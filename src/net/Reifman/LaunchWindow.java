262d83dd605bdd30694458406badf0e201fd57ac
Andrew Reifman-Packett <Andrew@Reifman.net>
2015-07-09 06:20:33 -0400

    Added draft order confirmation.

--- src/net/Reifman/LaunchWindow.java
+++ src/net/Reifman/LaunchWindow.java
@@ -22,149 +22,164 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-
 public class LaunchWindow {
 
+	JFrame window;
+	private static final int EXIT_ON_CLOSE = 0;
+	JPanel buttons;
+	JPanel running;
 
-  JFrame window;
-  private static final int EXIT_ON_CLOSE = 0;
-  JPanel buttons;
-  JPanel running;
+	ArrayList<Integer> temp;
 
-  ArrayList<Integer> temp;
+	public LaunchWindow() {
+		window = new JFrame();
+		buttons = new JPanel();
+		running = new JPanel();
 
-  public LaunchWindow() {
-    window = new JFrame();
-    buttons = new JPanel();
-    running = new JPanel();
+	}
 
-  }
+	public void createGUI() {
 
+		window.getContentPane().add(buttons);
+		JLabel lblOrder = new JLabel("Enter Draft Order");
+		JLabel lblRounds = new JLabel("How many rounds in tonights draft?");
+		final JRadioButton btnNormalDraft = new JRadioButton("6");
+		final JRadioButton btnFirstDraft = new JRadioButton("28");
 
+		final JTextField txtOrder = new JTextField(10);
 
-  public void createGUI() {
-   
-    window.getContentPane().add(buttons);
-    JLabel lblOrder = new JLabel("Enter Draft Order");
-    JLabel lblRounds = new JLabel("How many rounds in tonights draft?");
-    final JRadioButton btnNormalDraft = new JRadioButton("6");
-    final JRadioButton btnFirstDraft = new JRadioButton("28");
+		window.setTitle("Bbfbl Bot");
+		window.setSize(300, 200);
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(0);
 
-    final JTextField txtOrder = new JTextField(10);
+		JButton close = new JButton("Exit");
+		JButton start = new JButton("Start");
+		ButtonGroup group = new ButtonGroup();
+		group.add(btnFirstDraft);
+		group.add(btnNormalDraft);
+		buttons.setLayout(new FlowLayout());
+		buttons.add(lblOrder);
+		buttons.add(txtOrder);
+		buttons.add(lblRounds);
+		buttons.add(btnFirstDraft);
+		buttons.add(btnNormalDraft);
+		buttons.add(start);
+		buttons.add(close);
+		window.getContentPane().add(buttons);
 
-    window.setTitle("Bbfbl Bot");
-    window.setSize(300, 200);
-    window.setLocationRelativeTo(null);
-    window.setDefaultCloseOperation(0);
+		window.pack();
 
+		window.setVisible(true);
 
-    JButton close = new JButton("Exit");
-    JButton start = new JButton("Start");
-    ButtonGroup group = new ButtonGroup();
-    group.add(btnFirstDraft);
-    group.add(btnNormalDraft);
-    buttons.setLayout(new FlowLayout());
-    buttons.add(lblOrder);
-    buttons.add(txtOrder);
-    buttons.add(lblRounds);
-    buttons.add(btnFirstDraft);
-    buttons.add(btnNormalDraft);
-    buttons.add(start);
-    buttons.add(close);
-    window.getContentPane().add(buttons);
-    
+		close.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 
-    window.pack();
-   
-    window.setVisible(true);
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (btnFirstDraft.isSelected()) {
+					if (JOptionPane.showConfirmDialog(null,
+							"The order inputted is: " + txtOrder.getText()
+									+ ". Is that correct?", "WARNING",
+							JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+						temp = pullInfo(txtOrder, 28);
+					} else {
+						txtOrder.setText("");
+					}
 
+				} else if (btnNormalDraft.isSelected()) {
+					if (JOptionPane.showConfirmDialog(null,
+							"The order inputted is: " + txtOrder.getText()
+									+ ". Is that correct?", "WARNING",
+							JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+						temp = pullInfo(txtOrder, 6);
+					} else {
+						txtOrder.setText("");
+					}
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Please ensure you choose the number of rounds");
+				}
+				if (temp.size() > 1) {
+					window.setVisible(false);
+					window.dispose();
+					int rounds = temp.get(0);
+					temp.remove(0);
+					ArrayList<Integer> order = temp;
+					window.dispose();
+					window = new JFrame();
+					showRunningWindow();
+					connect(order, rounds);
+				}
+			}
+		});
 
-    close.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        System.exit(0);
-      }
-    });
+	}
 
-    start.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        if (btnFirstDraft.isSelected()) {
-          temp = pullInfo(txtOrder, 28);
-        } else if (btnNormalDraft.isSelected()) {
-          temp = pullInfo(txtOrder, 6);
-        } else {
-          JOptionPane.showMessageDialog(null, "Please ensure you choose the number of rounds");
-        }
-        if (temp.size() > 1) {
-          window.setVisible(false);
-          window.dispose();
-          int rounds = temp.get(0);
-          temp.remove(0);
-          ArrayList<Integer> order = temp;
-          window.dispose();
-          window = new JFrame();
-          showRunningWindow();
-          connect(order, rounds);
-        }
-      }
-    });
+	public ArrayList<Integer> pullInfo(JTextField txtOrder, int numRounds) {
+		ArrayList<Integer> returns = new ArrayList<Integer>();
+		ArrayList<Integer> temps = new ArrayList<Integer>();
+		String tempOrder = txtOrder.getText();
+		String[] temp = tempOrder.split("\\s+");
+		try {
+			returns.add(numRounds);
+			for (String t : temp) {
+				returns.add(Integer.parseInt(t));
+				temps.add(Integer.parseInt(t));
+			}
+		} catch (Exception e) {
+			if (txtOrder.getText().equals(""))
+				JOptionPane.showMessageDialog(null,
+						"Please enter a draft order");
+			else
+				JOptionPane.showMessageDialog(null,
+						"Please enter only numbers and spaces");
+			txtOrder.setText("");
+		}
 
-    
-  }
+		return returns;
+	}
 
-  public ArrayList<Integer> pullInfo(JTextField txtOrder, int numRounds) {
-    ArrayList<Integer> returns = new ArrayList<Integer>();
-    String tempOrder = txtOrder.getText();
-    String[] temp = tempOrder.split("\\s+");
-    try {
-      returns.add(numRounds);
-      for (String t : temp) {
-        returns.add(Integer.parseInt(t));
-      }
-    } catch (Exception e) {
-      if(txtOrder.getText().equals(""))
-        JOptionPane.showMessageDialog(null, "Please enter a draft order");
-      else
-        JOptionPane.showMessageDialog(null, "Please enter only numbers and spaces");
-      txtOrder.setText("");
-    }
+	private void connect(ArrayList<Integer> order, int rounds) {
 
-    return returns;
-  }
+		BotClass bot = new BotClass("BBFBL_Mod", order, rounds); // create new
+																	// bot and
+																	// name it
+		try {
+			bot.setVerbose(true);
+			bot.connect("irc.freenode.org"); // connect to freenode. Can be set
+												// to other IRC chat
+			// bot.sendMessage("nickserv", "IDENTIFY <password>"); //Once
+			// username is registered, this
+			// will be the password
+			bot.joinChannel("#bbfbl"); // what channel to join
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-  private void connect(ArrayList<Integer> order, int rounds) {
-   
-   
-    BotClass bot = new BotClass("BBFBL_Mod", order, rounds); // create new bot and name it
-    try {
-      bot.setVerbose(true);
-      bot.connect("irc.freenode.org"); // connect to freenode. Can be set to other IRC chat
-      // bot.sendMessage("nickserv", "IDENTIFY <password>"); //Once username is registered, this
-      // will be the password
-      bot.joinChannel("#bbfbl"); // what channel to join
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	public void showRunningWindow() {
 
-  public void showRunningWindow(){
-    
-    JLabel lblRunning = new JLabel("Running...");
-    JButton btnExit = new JButton("Exit");
-    window.setTitle("Bbfbl Bot");
-    window.setSize(300, 200);
-    window.setLocationRelativeTo(null);
-    window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    running.setLayout(new FlowLayout());
-    running.add(lblRunning);
-    running.add(btnExit);
-    window.getContentPane().add(running);
-    window.pack();
-    window.setVisible(true);
-    
-    btnExit.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        System.exit(0);
-      }
-    });
-  }
+		JLabel lblRunning = new JLabel("Running...");
+		JButton btnExit = new JButton("Exit");
+		window.setTitle("Bbfbl Bot");
+		window.setSize(300, 200);
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		running.setLayout(new FlowLayout());
+		running.add(lblRunning);
+		running.add(btnExit);
+		window.getContentPane().add(running);
+		window.pack();
+		window.setVisible(true);
+
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+	}
 }
