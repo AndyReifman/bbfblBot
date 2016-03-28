edc3dd12dd8bc14419e016bcca0888c8684da952
Andrew Reifman-Packett <Andrew@Reifman.net>
2016-03-28 16:28:07 -0400

    Updated comment in MainClass to include proper names.

--- src/net/Reifman/MainClass.java
+++ src/net/Reifman/MainClass.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 /**
 	* March 2014
 	* Copyright: 2014-2015
-	* Thanks to Misha Gale for helping guide me in the write direction and for writing the code that connects to a webpage
+	* Thanks to Michael Gale for helping guide me in the write direction and for writing the code that connects to a webpage
 	* @author Andrew Reifman-Packett
 */
 
